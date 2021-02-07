a96d3f382334ea3cc18223a9474f9d17bd8986f6
jora <nabilbakour0@gmail.com>
2021-02-07 17:16:15 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import logo from './logo.svg'; /*bdel hada l chi logo dialna*/ 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router,
+Route,
+Switch,
+Links, /* we use like this : import { link from "react-router-dom"; 
+ then inside the const we write : <link to="/path">linktext</link>}*/
+Redirect 
+} from "react-router-dom";
+
+//pages
+import MainPage from "./pages/Home";
+import NotFoundPage from "./pages/404";
+import LoginPage from "./pages/login";
+
+
+class App extends Component {
+  render()
+  {
+    return (
+    <Router>
+      <Switch>
+      <Route exact path="/" component={LoginPage}/>
+      <Route exact path="/home" component={MainPage} />
+      <Route exact path="/404" component={NotFoundPage}/>
+      <Redirect to="/404"/>
+
+      </Switch>
+    </Router>
+    );
+  }
 }
 
 export default App;
